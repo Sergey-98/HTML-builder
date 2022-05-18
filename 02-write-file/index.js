378b519cc62cba77c960c0f348fbe1136c436372
Sergey-98 <yatsenko1998@yandex.ru>
2022-05-18 22:31:17 +0400

    feat: complete 02-write-file section

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const stream = new fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+rl.write('Hello, press your message, please: \n');
+rl.on('line', (inp) => {
+  if (inp === 'exit') {
+    rl.write('\nThank you for your message!');
+    process.exit();
+  }
+  let final = inp + '\n';
+  stream.write(final);
+});
+
+rl.on('close', () => {
+  rl.write('\nThank you for your message!');
+  process.exit();
+});
