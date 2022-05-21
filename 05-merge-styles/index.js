7fdf1eee101819824962673ab66a463e3ed63c1a
Sergey-98 <yatsenko1998@yandex.ru>
2022-05-21 18:09:40 +0400

    feat: complete 05-merge-styles section

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = path.join(__dirname, 'project-dist');
+const streamWrite = new fs.createWriteStream(path.join(stream, 'bundle.css'));
+const styles = path.join(__dirname, 'styles');
+
+fs.readdir(styles, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log(err);
+  }
+  files.forEach((file) => {
+    let folderFile = path.join(styles, file.name);
+    if (file.isFile() && path.extname(folderFile) === '.css') {
+      const arr = [];
+      let readFile = new fs.ReadStream(folderFile, 'utf-8');
+      readFile.on('data', chunk => arr.push(chunk));
+      readFile.on('end', () => arr.forEach(elem => streamWrite.write(`${elem}\n`)));
+    }
+  });
+});
